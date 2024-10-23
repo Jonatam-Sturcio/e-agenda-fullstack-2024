e9fdb0c5d8d7e3fe53ae6540b9ba1f59189a8fa1
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-10-23 15:44:45 -0300

    Cadastro e HttpInterception

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,14 +4,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideAuthentication } from './core/auth/auth.provider';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideNotification } from './core/notificacao/notificacao.provider';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     provideAuthentication(),
+    provideNotification(),
   ],
 };
--- client/src/app/views/contatos/cadastrar/cadastro-contato.component.ts
+++ client/src/app/views/contatos/cadastrar/cadastro-contato.component.ts
@@ -1,12 +1,126 @@
+import { NgIf, NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router, RouterLink } from '@angular/router';
+import { NotificacaoService } from '../../../core/notificacao/notificacao.service';
+import { ContatoService } from '../services/contato.service';
+import {
+  ContatoInseridoViewModel,
+  InserirContatoViewModel,
+} from '../models/contato.models';
+import { PartialObserver } from 'rxjs';
 
 @Component({
   selector: 'app-cadastro-contato',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf,
+    RouterLink,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './cadastro-contato.component.html',
-  styleUrl: './cadastro-contato.component.scss'
 })
 export class CadastroContatoComponent {
+  public form: FormGroup;
 
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private contatoService: ContatoService,
+    private nofitificacaoService: NotificacaoService
+  ) {
+    this.form = this.fb.group({
+      nome: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(30),
+        ],
+      ],
+      telefone: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[1-9]{2} [0-9]{4,5}-[0-9]{4}$/),
+        ],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      empresa: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(30),
+        ],
+      ],
+      cargo: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(30),
+        ],
+      ],
+    });
+  }
+  get nome() {
+    return this.form.get('nome');
+  }
+
+  get telefone() {
+    return this.form.get('telefone');
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get empresa() {
+    return this.form.get('empresa');
+  }
+
+  get cargo() {
+    return this.form.get('cargo');
+  }
+  public gravar() {
+    if (this.form.invalid) {
+      this.nofitificacaoService.aviso(
+        'Por favor, preencha o formulário corretamente.'
+      );
+      return;
+    }
+    const inserirContato: InserirContatoViewModel = this.form.value;
+
+    const observer: PartialObserver<ContatoInseridoViewModel> = {
+      next: (contatoInserido) => this.processarSucesso(contatoInserido),
+      error: (erro) => this.processarFalha(erro),
+    };
+
+    this.contatoService.inserir(inserirContato).subscribe({});
+  }
+
+  private processarSucesso(contato: ContatoInseridoViewModel): void {
+    this.nofitificacaoService.sucesso(
+      `Contato "${contato.nome}" cadastrado com sucesso!`
+    );
+    this.router.navigate(['/contatos', 'listar']);
+  }
+  private processarFalha(erro: Error) {
+    this.nofitificacaoService.erro(erro.message);
+  }
 }
