73afa5a1826aa0333788855e99951e96643a4962
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-10-29 14:48:20 -0300

    Implementado teste crud em categorias

---
+++ client/cypress/e2e/Categorias/crud.cy.ts
@@ -0,0 +1,48 @@
+describe('Processo de redirecionamento para listar categorias', () => {
+  beforeEach(() => {
+    cy.visit('/');
+
+    cy.get('[data-cy=login').type('Cypress');
+
+    cy.get('[data-cy=senha]').type('Teste@123');
+
+    cy.get('[data-cy=submit]').click();
+
+    cy.wait(1000);
+
+    cy.get('[data-cy=categorias]').click();
+  });
+
+  it('Deve navegar até categorias', () => {
+    cy.contains('Listagem de Categorias');
+  });
+
+  it('Deve cadastrar uma nova categoria', () => {
+    cy.get('[data-cy=cadastrar]').click();
+
+    cy.get('[data-cy=titulo]').type('Categoria Cypress');
+
+    cy.get('[data-cy=confirmar]').click();
+
+    cy.contains('Categoria Cypress');
+  });
+
+  it('Deve editar uma categoria', () => {
+    cy.get('[data-cy=editar]').first().click();
+
+    cy.get('[data-cy=titulo]').clear();
+    cy.get('[data-cy=titulo]').type('Categoria Cypress Editado');
+
+    cy.get('[data-cy=confirmar]').click();
+
+    cy.contains('Categoria Cypress Editado');
+  });
+
+  it('Deve excluir uma categoria', () => {
+    cy.get('[data-cy=excluir]').first().click();
+
+    cy.get('[data-cy=confirmar]').click();
+
+    cy.contains('Categoria Cypress Editado').should('not.exist');
+  });
+});
